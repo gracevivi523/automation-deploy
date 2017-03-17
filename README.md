1cc29dc3d0676c0ab7274f8cb3a6a2b3271e038d
gracevivi523 <gw3155530@163.com>
2017-03-17 21:01:43 +1100

    modify install script

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # automation-deploy
 automation-deploy for rea interview
+
+Main idea for the automation deployment:
+	- Use knife-ec2 plugin to automatically launch an EC2 instance embeded an Amazon AMI Linux image.
+	- Use chef to make automated-deploy the provided application, here, I used api.chef.io as my hosted chef server, so it saves the trouble making chef server.
+	- I already uploaded chef cookbook in chef server, in the cookbook, I did the following things which you can also review in my git repo after you download later.
+		o install git, in order to checkout the git code of simple-sinatra-app
+		o git checkout simple-sinatra-app
+		o set iptables rules, redirect traffic from port 80 to port 3000
+		o start sinatra service, listening on port 3000. (there is a small issue that I can't start it on port 80 because port is reserved under 1000,so I just use iptables to redirect to port 8o)
+
+My working environment:
+	- macOS Sierra (version 10.12.3)
+
+Some important notes before you run the scripts:
+	- Preferred review workstation: macOS
+	- Make sure your workstation have access to the Internet.
+	- Please subsititute your own working username for $username used in the following steps.for example, mine is "/Users/grace"
+	- I already get the following things done for authentification for chef server and AWS, so you dont have to do anything for authetificaiton for chef server or AWS.
+		o user.gem was created for connecting to chef server.
+		o a new user named 'knife' was created in AWS and knife.pem stored.
+		o knife.rb has been created for you to connect to hosted chef server.
+		o aws access and secret access key has been created and stored.
+	
+For now, you will be able to follow on the following steps to start your amazing journey.
+
+1. Installation
+
+ 1).	Please install git on your workstation firstly, move on to 2) if you already have one.
+ 2).	Make sure your working account has sudo permission. Please add it to /etc/sudoers if not, don't use root as your working account which is not safe;
+ 3).	Go to your working directory, $HOME, let's say /Users/grace;
+ 4).	Checkout my git source by using the following command:
+	
+	$ git clone https://github.com/gracevivi523/automation-deploy.git
+
+	Now, you should see your working directory like this
+	$ $HOME/automation-deploy
+
+ 5).	Run the following shell script to install pre packages:
+	
+	$ cd /Users/$username/automation-deploy
+	$ sh install_pre_packages.sh
+	
+	This installation may take a while, and this script will install packages below:
+		- homebrew, rbenv, ruby-build, ruby, knife-ec2, chef
+
+2. Prepare work directory and credentials for aws and chef server
+
+ 1).	Run the following shell script to copy credentials to dest on your workstation
+
+	$ cd /Users/$username/automation-deploy/scripts/
+	$ sh prepare_work_directory.sh
+	
+3. Create EC2 instance and deploy the provided application 'simple-sinatra-app'
+ 1).	Run create_ec2_instance.sh
+	$ cd /User/$username/automation-deploy/scripts/
+	$ sh create_ec2_instance.sh
+
+For now, you'll be able to visit it "http://ip" in your browser, here, replace ip with public ipV4 of created ec2 instance, you can find the public ipv4 on the output when you run this deployment.
+
+Feel free to contact me if you run into any issues.
--- scripts/install_pre_packages.sh
+++ scripts/install_pre_packages.sh
@@ -1,67 +1,86 @@
-#!/bin/bash
+#/usr/bin/env bash
+set -e
 
-# get work username and group name
-#username = `whoami`
-#groupname = `id -Gn $username | awk '{print $1}'`
+username=`whoami`
+groupname=`id -Gn $username | awk '{print $1}'`
 
-# install rvm for a multi-user
-#install_rvm()
-#{
-#	print "****** install rvm start ******"
-#	curl -sSL https://get.rvm.io | sudo bash -s stable
-#	print "****** install rvm finish ******"
-#	# change /usr/local/rvm to to your work user permission
-#	sudo chown -R $username:$groupname /usr/local/rvm
-#	source /etc/profile
-#}
-
-install_homebrew()
+# install rvm and ruby for a multi-user
+install_rvm_ruby()
 {
-	print "install homebrew start ******"
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	print "install homebrew finish ******"
+	echo "****** install rvm start ******"
+	curl -sSL https://get.rvm.io | sudo bash -s stable
+	# change /usr/local/rvm to to your work user permission
+	sudo chown -R $username:$groupname /usr/local/rvm
+	# add your username into rvm group
+	sudo dseditgroup -o edit -a $username -t user rvm
+	test -f /etc/profile.d/rvm.sh && source /etc/profile.d/rvm.sh
+
+        # Load RVM into a shell session *as a function*
+        # Loading RVM *as a function* is mandatory
+        # so that we can use 'rvm use <specific version>'
+        if [[ -s "/$HOME/.rvm/scripts/rvm" ]] ; then
+                # First try to load from a user install
+                source "$HOME/.rvm/scripts/rvm"
+                echo "using user install $HOME/.rvm/scripts/rvm"
+        elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+                # Then try to load from a root install
+                source "/usr/local/rvm/scripts/rvm"
+                echo "using root install /usr/local/rvm/scripts/rvm"
+        else
+                echo "ERROR: An RVM installation was not found.\n"
+                exit 1
+        fi
+	echo "****** install rvm finish ******\n\n\n"
+
+	echo "****** install ruby 2.2.6 and above start ******"
+#	#upgrade ruby to 2.2.6
+#	rvm requirements
+	rvm=`which rvm`
+	$rvm install 2.2.6
+	$rvm use 2.2.6 --default
+	echo "****** your current ruby version is ******"
+	ruby=`which -a ruby  | grep "rvm"`
+	$ruby -v
+	gem=`which -a gem | grep rvm`
+	$gem -v
+	echo "****** install ruby 2.2 and above finish ******\n\n\n"
 }
 
-install_ruby()
+install_homebrew_ruby()
 {
-	print "****** install ruby 2.2 and above start ******"
+	echo "****** install homebrew start ******"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo "****** install homebrew finish ******\n\n\n"
+	echo "****** install ruby 2.2.6 start ******"
 	brew install ruby-build
 	brew install rbenv
 	rbenv install 2.4.0
 	rbenv global 2.4.0
-#	#upgrade ruby to 2.2.6
-#	rvm requirements
-#	rvm install 2.2.6
-#	rvm use 2.2.6 --default
-	print "****** install ruby 2.2 and above finish ******"
-	print "****** your current ruby version is \n ******"
+	echo "eval "$(rbenv init -)" >> /etc/profile
+	source /etc/profile
+	echo "****** install ruby 2.2.6 finish ******"
+	echo "****** your current ruby version is:"
 	ruby -v
 	gem -v
 }
 
-install_knife-ec2()
+install_knife_ec2()
 {
-	print "****** install knife-ec2 plugin start ******"
+	echo "****** install knife-ec2 plugin start ******"
 	sudo gem install knife-ec2
-	print "****** install knife-ec2 plugin finish *****"
+	echo "****** install knife-ec2 plugin finish *****\n\n\n"
 }
 
 install_chef()
 {
-	print "******* install chef start ******"
+	echo "******* install chef start ******"
 	curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable
-	print "******* install chef finish ******"
-	print "****** your current chef version is \n ******"
+	echo "******* install chef finish ******"
+	echo "****** your current chef version is******"
 	chef --version
 }
 
-install_git()
-{
-        print "******* install git start ******"
-	brew install git
-        print "******* install git finish ******"
-        print "****** your current chef version is \n ******"
-        git --version
-}
-
-
+#install_rvm_ruby
+install_homebrew_ruby
+install_chef
+install_knife_ec2
